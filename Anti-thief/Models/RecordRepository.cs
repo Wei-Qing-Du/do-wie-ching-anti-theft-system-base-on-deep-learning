6455ae0605070b319f822a0d79097883d3f9a515
Willy <willy.du@ulictek.com>
2021-08-06 16:11:06 +0800

    Add development website to show situation with asp.net

---
+++ Anti-thief/Models/RecordRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anti_thief.Models
+{
+    public class RecordRepository
+    {
+    }
+}
